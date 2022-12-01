820302b0cbbe243fddbb8fb35af2321e70b41c1f
wallabyway <bealester@gmail.com>
2022-12-01 13:01:58 -0800

    added new commands for file-copy

--- api.mjs
+++ api.mjs
@@ -1,32 +1,19 @@
 import fetch  from 'node-fetch';
-import { exit } from 'process';
-import * as stream from 'stream';
 
 const _header = (token) => { return {"Content-Type":"application/json", "Authorization": `Bearer ${token}`}};
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
-export class api {
-	static async login(key, secret) {
-		const url = `https://developer.api.autodesk.com/authentication/v1/authenticate`;
-		const header = { 'Content-Type': 'application/x-www-form-urlencoded' }
-		//const scope = "account:read account:write bucket:create bucket:read bucket:update bucket:delete data:read data:write data:create data:search user:read user:write user-profile:read viewables:read";
-		const scope2 = "data%3Acreate%20account%3Aread%20account%3Awrite%20bucket%3Aread%20user%3Aread%20viewables%3Aread%20data%3Aread%20user-profile%3Aread%20data%3Asearch"; 
-		const body = `grant_type=client_credentials&client_id=${key}&client_secret=${secret}&scope=${scope2}`;
-		//const body = `grant_type=client_credentials&client_id=${key}&client_secret=${secret}&scope=${encodeURIComponent(scope)}`;
-		const resp = await fetch( url, { method: 'POST', headers: header, body: body });
-		const json = await resp.json();
-		return json.access_token;
-	}
-
-	static async listAccounts(token) {
+export class projects {
+	static async showHubs(token) {
 		// USES 3-lEGGED
 		//const url = `https://developer.api.autodesk.com/ea-api/v1/project_entitlements?service_categories=next_gen%2Cadmin&current_service_type=hq&include_services=false&include_containers=false&limit=400`;
 		const url = `https://developer.api.autodesk.com/ea-api/v1/account_entitlements?service_categories=next_gen,classic,admin&include_services=true&limit=200&offset=0`;
 		const data = await (await fetch(url, {headers: _header(token)} )).json();
-		if (data.message) { console.table(data); exit(1)}
+		if (data.message) { console.table(data); process.exit(1)}
 		return data.accounts.map( item => { return { id: item.account_id, name: item.account_display_name, region: item.region }});		
 	}
 
-	static async listProjects(token, account_id) {
+	static async list(token, account_id) {
 		// USES 2-lEGGED
 		//const url = `https://developer.api.autodesk.com/bim360/admin/v1/accounts/${account_id}/projects?limit=500&sort[]=name+asc&filter[type]=-&filter[platform]=bim360`;
 		const url = `https://developer.api.autodesk.com/hq/v1/accounts/${account_id}/projects`;
@@ -34,16 +21,59 @@ export class api {
 		return data.map( item => { return { id: item.id, name: item.name }});		
 	}
 
-
-	static async postEmptyProject(token, account_id, params) {
+	static async createEmpty(token, account_id, user_email, params) {
 		// USES 2-lEGGED
 		const url = `https://developer.api.autodesk.com/hq/v1/accounts/${account_id}/projects`
 		const res = await fetch(url, { method: 'POST', body:JSON.stringify(params), headers: _header(token) });
-		const json = await res.json();
-		return json;
+		const project_id = (await res.json()).id;
+		//wait project status = active: 
+		// https://developer.api.autodesk.com/hq/v1/accounts/:account_id/projects/:project_id
+		let status = "pending";
+		while (status != "active") {
+			await delay(2000);
+			const res1 = await fetch(`https://developer.api.autodesk.com/hq/v1/accounts/${account_id}/projects/${project_id}`, { headers: _header(token) });
+			status = (await res1.json());
+			console.log(res1);
+			console.log(`Waiting on new Project Status: ${status}`)
+		};
+		await delay(2000);
+		const folder_id = await this.getTopFolderID(token, account_id, project_id)
+		const company_id = await this.getCompanyIDFromProject(token, account_id, project_id);
+		const user_id = await this.assignSelfToProject(token, account_id, company_id, user_email );
+		return [project_id, folder_id];
 	}
 
-	static async patchTemplate(token, template_project_id, dst_project_id) {
+	static async assignSelfToProject(token, account_id, company_id, user_email) {
+		const body = {
+			"role": "project_admin",
+			"service_type": "doc_manager",
+			"company_id":company_id,
+			"email":user_email
+		};
+		const res = await fetch(`https://developer.api.autodesk.com/hq/v1/accounts/${account_id}/projects/${project_id}/users`, { 
+			method: 'POST', headers: { headers: _header(token) }, body: JSON.stringify(body) });
+		const user = (await res.json());
+		await delay(2000);
+		return user.id;
+	}
+
+	static async getTopFolderID(token, account_id, project_id) {
+		const res = await (await fetch(`https://developer.api.autodesk.com/project/v1/hubs/${account_id}/projects/${project_id}/topFolders&projectFilesOnly=true&excludeDeleted=true`, { headers: _header(token) })).json();
+		debugger;
+		return res.data[0].id;
+	}
+
+	static async getCompanyIDFromProject(token, account_id, project_id) {
+		const res = await (await fetch(`https://developer.api.autodesk.com/hq/v1/accounts/${account_id}/projects/${project_id}/companies?limit=1&offset=0`, { headers: _header(token) })).json();
+		debugger;
+		return res[0].id;
+	}
+
+	
+
+		
+
+	static async copyTemplate(token, template_project_id, dst_project_id) {
 		// USES 3-lEGGED
 		const body = {
 		    "services": [
@@ -60,35 +90,31 @@ export class api {
 		if (res.status != 202) 
 			console.log(res.status,res.statusText);
 		const out = await res.json();
+		await delay(10000);
 		return out;
 	}
 
-	static async patchUserRoles(token, account_id, g_spreadsheet_url) {
+	static async addUserRoles(token, token2, account_id, project_id, g_spreadsheet_url) {
 
-		// Fetch User roles from Google spreadsheet
-		// example spreadsheet:
-		//  API: https://sheets.googleapis.com/v4/spreadsheets/1yxfWCfmhq-j_MC06iAoKsxNBummpjsleC6a9Ad5M6aM/values/roles?key=AIzaSyB-P6t82sJcClwhyCRhDNsiMdBTNH0BCyo
-		const json = await (await fetch(g_spreadsheet_url)).json();
-		json.values.slice(1).forEach( async i => await patchUser(i, i.user_id) );
-		
-		// import list of users into BIM 360/ACC
-		async function patchUser(user_id) {
-			const url = `https://developer.api.autodesk.com/hq/v2/accounts/${account_id}/users/${user_id}`;
-			const res = await fetch(url, { method: 'POST', body:JSON.stringify(bimuserjson), headers: _header(token) });
-			return await res.json();
-		}
-	}
+		const industryroles = await (await fetch(`https://developer.api.autodesk.com/hq/v2/accounts/${account_id}/projects/${project_id}/industry_roles`, {headers: _header(token)})).json();
+		const role2idmap = Object.fromEntries(industryroles.map(e => [e.name, e.id]))
+		console.table(role2idmap);		
 
-	static async addUsersToProject(token, account_id, project_id, g_spreadsheet_url) {
-
-		const bimuserjson = await (await fetch(g_spreadsheet_url)).json();
+		const roles = this.pivotJson(await (await fetch(g_spreadsheet_url)).json());
+		roles.forEach(i => {
+			i.industry_roles = [role2idmap[i.industry_roles]];
+			i.services = {document_management: { access_level: "user" }};
+		})
+		console.table(roles);
 
 		// Adds users (project admin and project user) to a project
 		const url = `https://developer.api.autodesk.com/hq/v2/accounts/${account_id}/projects/${project_id}/users/import`;
-		const res = await fetch(url, { method: 'POST', body:JSON.stringify(bimuserjson), headers: _header(token) });
+		const res = await fetch(url, { method: 'POST', body:JSON.stringify(roles), headers: _header(token) });
 		const json = await res.json();
+		const email2accountid = Object.fromEntries(json.success_items.map(e => [e.email, e.account_id]));
+		if (json.failure_items.length) json.failure_items = json.failure_items[0].errors[0]
+		if (json.success_items.length) json.success_items = `${json.success_items[0].email}, etc... ${json.success_items.length} users added`
 		return json;
-
 	} 
 
 	static async importUsersFromGoogleSheets(token, account_id, g_spreadsheet_url) {
@@ -104,49 +130,54 @@ export class api {
 		const g_spreadsheet_url = `https://sheets.googleapis.com/v4/spreadsheets/${g_spreadsheet}/values/${g_tabname}?key=${g_key}`
 		*/
 
-		const userjson = await (await fetch(g_spreadsheet_url)).json();
-		const bimuserjson = pivotJson(userjson);
+		const users = this.pivotJson(await (await fetch(g_spreadsheet_url)).json());
 
 		// import list of users into BIM 360/ACC
 		const url = `https://developer.api.autodesk.com/hq/v1/accounts/${account_id}/users/import`;
-		const res = await fetch(url, { method: 'POST', body:JSON.stringify(bimuserjson), headers: _header(token) });
+		const res = await fetch(url, { method: 'POST', body:JSON.stringify(users), headers: _header(token) });
 		const json = await res.json();
-		return json;
-
-		function pivotJson(json) {
-			let keys = json.values[0];
-			let valueArray = json.values.slice(1);
-			var oObj = [];
-			valueArray.forEach( row => {
-				var iObj = {};
-				keys.forEach( (column,i) => iObj[column] = row[i])
-				oObj.push(iObj);
-			})
-			return oObj;
-		}
-
-		function pivotJson2(json) {
-			let keys = json.values[0];
-			let valueArray = json.values.slice(1);
-			var oObj = [];
-			valueArray.forEach( row => {
-				var iObj = [];
-				keys.forEach( (column,i) => {
-					let obj = {}
-					obj[column] = row[i];
-					iObj.push(obj);
-				})
-				oObj.push(iObj);
-			})
-			return oObj;
-		}
-		
+		return json;		
 	}
-	
-	static async copyfile(token, src, dst) {
-		// https://github.com/node-fetch/node-fetch/blob/4d944365dfeff3ab0cb8b978c1e6fa9e3b640e2e/test/test.js#L679-L696
-		const resp = await fetch(src, header(token));
-		body = resp.body.pipe(new stream.PassThrough());
-		await fetch(dst, { method: 'PUT' , body })
+
+	static pivotJson(json) {
+		let keys = json.values[0];
+		let valueArray = json.values.slice(1);
+		var oObj = [];
+		valueArray.forEach( row => {
+			var iObj = {};
+			keys.forEach( (column,i) => iObj[column] = row[i])
+			oObj.push(iObj);
+		})
+		return oObj;
 	}
 }
+
+/*
+		/*
+		// Sometimes BIM360 is partially updated and roles are missing.  Kick the server, by triggering a patch to each user. 
+		async function patchUserRole(user, email2accountid) {
+			const user_id = email2accountid[user.email];
+			const url = `https://developer.api.autodesk.com/hq/v2/accounts/${account_id}/projects/${project_id}/users/${user_id}`;
+			const body = {industry_roles: user.industry_roles , "company_id": "69e416fe-5dad-46a2-ad29-221aa00452a0"};
+			const res = await fetch(url, { method: 'PATCH', body:JSON.stringify(body), headers: _header(token) });
+			const json = await res.json();
+			console.log(json);
+		}		
+		roles.forEach( async user => await patchUserRole(user, email2accountid));
+
+	static async addUsers_old(token, account_id, g_spreadsheet_url) {
+
+		// Fetch User roles from Google spreadsheet
+		// example spreadsheet:
+		//  API: https://sheets.googleapis.com/v4/spreadsheets/1yxfWCfmhq-j_MC06iAoKsxNBummpjsleC6a9Ad5M6aM/values/roles?key=AIzaSyB-P6t82sJcClwhyCRhDNsiMdBTNH0BCyo
+		const json = await (await fetch(g_spreadsheet_url)).json();
+		json.values.slice(1).forEach( async i => await patchUser(i, i.user_id) );
+		
+		// import list of users into BIM 360/ACC
+		async function patchUser(user_id) {
+			const url = `https://developer.api.autodesk.com/hq/v2/accounts/${account_id}/users/${user_id}`;
+			const res = await fetch(url, { method: 'POST', body:JSON.stringify(bimuserjson), headers: _header(token) });
+			return await res.json();
+		}
+	}
+*/
--- cli.mjs
+++ cli.mjs
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import { api } from './api.mjs';
+import { projects } from './api.mjs';
+import {auth} from './auth.mjs';
+import {fileUtils} from './fileUtils.mjs';
+
+const { TOKEN2, TOKEN3, ACCOUNT_ID } = process.env;
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 /*
 Commands:
@@ -16,21 +21,22 @@ Commands:
   help [command]                                       display help for command
 */
 
-const { TOKEN2, TOKEN3, ACCOUNT_ID } = process.env;
-if (!TOKEN2 && (process.argv[2]!="login")) {
+
+if (process.argv[2]=="login") {
+	const server = auth.startMiniServer();
+} else if (!TOKEN2) {
     console.warn('TOKEN is missing.  Please login and set the TOKEN environment variable. ie. export TOKEN2=1234; export TOKEN3=1234');
     process.exit(1);
 }
-
 
 program
 .command('login <key> <secret>')
 .description('gets 2-legged with forge key/secret + get 3-legged access tokens via browser and BIM360/ACC login page')
 .action(async (key, secret) => {
-  child_process.exec('open http://localhost:8080/');
-  const token = await api.login(key, secret);
-  console.log(`2 legged login successful.  now add TOKEN2 to your environment, like this...`);
-  console.log(`export TOKEN2=${token};`);
+	auth.launchBrowserForLogin();
+	const token = await auth.login_twolegged(key, secret);
+	console.log(`2 legged login successful.  now add TOKEN2 to your environment, like this...`);
+	console.log(`export TOKEN2=${token};`);
 });
 
 
@@ -38,7 +44,7 @@ program
   .command('listaccounts')
   .description('list all ACC/BIM 360 account IDs.  The new Project you will create, will go under this Account ID')
   .action(async () => {
-    console.table(await api.listAccounts(TOKEN3));
+    console.table(await projects.showHubs(TOKEN3));
 	console.log('export ACCOUNT_ID= <pick one^^^>');
   });
 
@@ -47,39 +53,51 @@ program
   .command('listprojects')
   .description('list BIM 360 Project Templates.  You will create a new Project from one of these existing Project templates')
   .action(async () => {
-    console.table(await api.listProjects(TOKEN2, ACCOUNT_ID));
+    console.table(await projects.list(TOKEN2, ACCOUNT_ID));
 });
+
+program
+  .command('masterCreate <project_name> <template_project_id> <srcFolderURL> <dstFolderURL>')
+  .description('list BIM 360 Project Templates.  You will create a new Project from one of these existing Project templates')
+  .action(async (project_name, template_project_id, srcFolderURL, dstFolderURL) => {
+	defaults.name = project_name;
+	const empty_project_id = await projects.createEmpty(TOKEN2, ACCOUNT_ID, defaults);
+	await projects.assignSelfToProject(TOKEN2, ACCOUNT_ID, user_id);
+	await projects.copyTemplate(TOKEN3, template_project_id, empty_project_id);
+	await fileUtils.copyFiles(TOKEN2, srcFolderURL, dstFolderURL);
+	console.log("finished");
+});
+{
+	static async masterCreate(token, account_id, params, template_project_id) {
+
+	}
+
+
+}
+
+const defaults = {
+	"service_types":"inSight", //doc_manager
+	"start_date": "2023-05-02",
+	"end_date": "2024-04-03",
+	"project_type": "Wind Farm",
+	"value": 3000,
+	"city": "New York",
+	"state_or_province": "New York",
+	"postal_code": "10011",
+	"country": "United States",
+	"timezone": "America/New_York",
+	"language": "en",
+	"currency": "USD"
+};	
 
 
 program
   .command('createproject <project_name>')
   .description('create a new Project, returns a projectID')
   .action(async (project_name) => {
-	const defaults = {
-		"name": project_name,
-		"service_types":"inSight", //doc_manager
-		"start_date": "2023-05-02",
-		"end_date": "2024-04-03",
-		"project_type": "Wind Farm",
-		"value": 3000,
-		"city": "New York",
-		"state_or_province": "New York",
-		"postal_code": "10011",
-		"country": "United States",
-		"timezone": "America/New_York",
-		"language": "en",
-		"currency": "USD"
-		/*
-		"job_number": "0219-01",
-		"address_line_1": "The Fifth Avenue",
-		"address_line_2": "#301",
-		"construction_type": "Wind Farm",
-		"contract_type": "Design-Bid",
-		*/
-	};	
-	const result = await api.postEmptyProject(TOKEN2, ACCOUNT_ID, defaults);
-    console.table(result);
-    console.log(`created project.  Project_ID is: ${result.id}`);
+	defaults.name = project_name;
+	const project_id = await projects.createEmpty(TOKEN2, ACCOUNT_ID, defaults);
+    console.log(`created project.  Project_ID is: ${project_id}`);
 });
 
 
@@ -87,7 +105,7 @@ program
   .command('copytemplate <new_project_id> <template_project_id>')
   .description('copy template folders/roles, into new Project')
   .action(async (new_project_id, template_project_id) => {
- 	const res = await api.patchTemplate(TOKEN3, template_project_id, new_project_id);
+ 	const res = await projects.copyTemplate(TOKEN3, template_project_id, new_project_id);
     console.table(res);
  });
 
@@ -95,33 +113,27 @@ program
   .command('importusers <g_spreadsheet_url>')
   .description('import users from google spreadsheet, into a bim360 hub')
   .action(async (g_spreadsheet_url) => {
- 	const res = await api.importUsersFromGoogleSheets(TOKEN2, ACCOUNT_ID, g_spreadsheet_url);
+ 	const res = await projects.importUsersFromGoogleSheets(TOKEN2, ACCOUNT_ID, g_spreadsheet_url);
     console.table(res);
  });
 
  program
-  .command('adduserstoproject <project_id> <g_spreadsheet_url>')
-  .description('Adds users (project admin and project user) to a project, from google spreadsheet')
+  .command('assignusers <project_id> <g_spreadsheet_url>')
+  .description('Assign users to a project, from google spreadsheet')
   .action(async (project_id, g_spreadsheet_url) => {
- 	const res = await api.addUsersToProject(TOKEN2, ACCOUNT_ID, project_id, g_spreadsheet_url);
+ 	const res = await projects.addUserRoles(TOKEN3, TOKEN2, ACCOUNT_ID, project_id, g_spreadsheet_url);
     console.table(res);
  });
-
- program
- .command('patchuserroles <g_spreadsheet_url>')
- .description('patch user roles, from google spreadsheet, into bim360')
- .action(async (g_spreadsheet_url) => {
-	const res = await api.patchUserRoles(TOKEN2, ACCOUNT_ID, g_spreadsheet_url);
-   console.table(res);
-});
 
 
 
 program
-  .command('copyfiles <template_project_id> <new_project_id>')
-  .description('recursively copy files from template folder into destination folder')
-  .action((template_project_id, new_project_id) => {
-    console.log(`recursively copied files from Template Folder ${template_project_id} to Project Folder ${new_project_id}... done`);
+  .command('copyfiles <srcFolderURL> <dstFolderURL>')
+  .description('copy files in root folder of template into destination project root folder')
+  .action(async (srcFolderURL, dstFolderURL) => {
+	const res = await fileUtils.copyFiles(TOKEN2, srcFolderURL, dstFolderURL);
+    console.table(res);
+    console.log(`finished copying files`);
   });
 
 
@@ -129,29 +141,9 @@ program
   .command('createworksharing <folder_id>')
   .description('create a new Revit cloud worksharing file in a folder')
   .action((folder_id) => {
-    console.log(`worksharing Revit file created into folderID ${folder_id}`);
+    console.log(`WIP - worksharing Revit file created into folderID ${folder_id}`);
   });
 
 program.parse(process.argv);
 
 
-// --------------------------------------
-// activate a mini server, for login page
-import http from 'http';
-import fs from 'fs';
-import url from 'url';
-import child_process from 'child_process';
-
-if (process.argv[2]=="login") {
-	const server = http.createServer((req, res) => {
-		const params = url.parse(req.url, true).query;
-		if (params.access_token) {
-			console.log(`export TOKEN3=${params.access_token}`);
-			process.env['TOKEN3']=params.access_token;
-			process.exit(1);
-		}
-		res.writeHead(200, { 'content-type': 'text/html' });
-		fs.createReadStream('index.html').pipe(res);
-	});
-	server.listen(8080);
-}
